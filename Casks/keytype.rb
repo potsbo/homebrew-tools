7ae03456d4ce4d7d8fef130a21abc1a0b66e1fc8
Shimpei Otsubo <potsbo@gmail.com>
2017-06-17 18:46:39 +0900

    Add keytype

---
+++ Casks/keytype.rb
@@ -0,0 +1,8 @@
+cask 'keytype' do
+  version '0.1.0'
+  sha256 '350fe974367ccc402abb34613a0689523dd6f42b77e4ef189929b4abe8aca0ff'
+  url "https://github.com/potsbo/keytype/releases/download/v#{version}/KeyType-sierra.zip"
+  name 'KeyType'
+  homepage 'https://github.com/potsbo/KeyType'
+  app 'KeyType.app'
+end
